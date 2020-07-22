dce6b058a929aa102cc3aa95061c0f270b900bfc
Okey Obiajunwa <okey.obia@gmail.com>
2020-07-22 18:14:04 -0400

    initial commit

---
+++ src/main/java/com/okeyobia/petclinik/SpringmvcPetClinikApplication.java
@@ -0,0 +1,13 @@
+package com.okeyobia.petclinik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcPetClinikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcPetClinikApplication.class, args);
+	}
+
+}
